df06002eb8514ee869c329b8d5d1ce2881ea655a
macmillianzungene <macmillianzungene@gmail.com>
2024-08-28 17:16:26 +0200

    moved to images

---
+++ registration.js
@@ -0,0 +1,11 @@
+document
+  .getElementById("register-form")
+  .addEventListener("submit", function (event) {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      event.preventDefault(); // Prevent form submission
+    }
+  });
